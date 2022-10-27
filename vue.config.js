137d9cade8afe00e75d13bbac33cd67e7857388a
Vika Marina <v.marina@idaproject.com>
2022-10-27 20:47:45 +0300

    feat(deploy)

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/game-app/'
-        : '/',
+    publicPath: '/game-app/',
     transpileDependencies: true,
     css: {
         loaderOptions: {
